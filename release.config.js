fdce181cb74165b36e56e7d7ab15cbaceb78b844
Demola <ademoladebayo@gmail.com>
2022-05-07 12:37:31 +0100

    fix: semantic-release description

--- release.config.js
+++ release.config.js
@@ -2,9 +2,9 @@ module.exports = {
   branches: "main",
   repositoryUrl: "https://github.com/ademola-adebayo/react-cicd-gha",
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm', 
-    '@semantic-release/github'
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github"
   ]
-}
+};
