2b860433712e30bd6b8784e3cb1d192233990e06
Demola <ademoladebayo@gmail.com>
2022-05-08 01:57:37 +0100

    feat: semantic-release

--- release.config.js
+++ release.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  branches: "main",
-  repositoryUrl: "https://github.com/ademola-adebayo/react-cicd-gha",
-  plugins: [
+  "branches": ["main"],
+  // repositoryUrl: "https://github.com/ademola-adebayo/react-cicd-gha",
+  "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    // "@semantic-release/npm",
+    "@semantic-release/changelog",
+    ["@semantic-release/npm", { "npmPublish": false}],
+    "@semantic-release/github"
+    ["@semantic-release/git",
+    {
+      "assets": ["package.json", "package-lock.json", "compiled/index.js", "CHANGELOG.md"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
     "@semantic-release/github"
   ]
 };
