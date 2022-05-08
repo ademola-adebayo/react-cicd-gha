bf6fec1b7bad4c6acb128b2eb5dcca9e80a8e8e2
Demola <ademoladebayo@gmail.com>
2022-05-08 18:53:20 +0100

    Testing Cutom Actions

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -928,13 +928,23 @@ const github = __webpack_require__(469);
 
 try {
   // throw new Error("some error message");
+
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
+
   const name = core.getInput('who-to-greet');
+  core.setSecret(name);
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
-
+  
+  core.startGroup('Logging github')
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message)
 }
