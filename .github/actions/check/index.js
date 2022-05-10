cf16a5bff7058092eef679700c1da8f3b6af2a36
Demola <ademoladebayo@gmail.com>
2022-05-10 23:45:16 +0100

    test: testing
    just testing custom actions

---
+++ .github/actions/check/index.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core');const github = require('@actions/github');
+
+(async () => {    
+  try {        
+    core.notice('Check File Action called!!!');    
+  } catch (error) {        
+    core.setFailed(error.message);    
+  }})();
