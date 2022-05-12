68435e745a51435ab3da3397d8a195740c0c6fc3
Demola <ademoladebayo@gmail.com>
2022-05-12 07:36:08 +0100

    test: testing
    testing custom actions

---
+++ .github/actions/custom/index.js
@@ -0,0 +1,35 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+(async () => {
+  const token = core.getInput("token");
+  const owner = core.getInput("owner");
+  const repo = core.getInput("repo");
+  const run_id = core.getInput("run_id");
+  const attempt_number = core.getInput("attempt_number");
+
+  // const assignees = core.getInput("assignees");
+
+  const octokit = github.getOctokit(token);
+
+  const response = await octokit.rest.actions.getWorkflowRunAttempt({
+    owner,
+    repo,
+    run_id,
+    attempt_number
+  });
+
+  const { data } = response;
+
+  const { context = {} } = github;
+
+  core.setOutput("DATA", JSON.stringify(data));
+  core.setOutput("repo", repo);
+  core.startGroup("Logging github");
+  console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.startGroup("Logging github context");
+  console.log(JSON.stringify(context, null, "\t"));
+  core.endGroup();
+})();
