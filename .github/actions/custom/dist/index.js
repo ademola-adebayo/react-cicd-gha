c81c6e8a2c90da0f3b4c9b4b04a9554bbaf230d3
Demola <ademoladebayo@gmail.com>
2022-05-13 15:18:29 +0100

    test: testing

--- .github/actions/custom/dist/index.js
+++ .github/actions/custom/dist/index.js
@@ -3937,9 +3937,10 @@ exports.RequestError = RequestError;
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 const { Octokit } = __webpack_require__(448);
-(async () => {
+
+async function run() {
   try {
-    const token = core.getInput("GITHUB_TOKEN");
+    const token = core.getInput("token");
     const owner = core.getInput("owner");
     const repo = core.getInput("repo");
     const run_id = core.getInput("run_id");
@@ -3952,37 +3953,40 @@ const { Octokit } = __webpack_require__(448);
     //   run_id,
     //   attempt_number,
     // });
-    const octokit = new Octokit({ auth: token });
-    const response = await octokit.request(
-      "GET /repos/{owner}/{repo}/actions/runs/{run_id}/attempts/{attempt_number}",
-      {
-        owner,
-        repo,
-        run_id,
-        attempt_number,
-      }
-    );
+    // const octokit = new Octokit({ auth: token });
+    // const response = await octokit.request(
+    //   "GET /repos/{owner}/{repo}/actions/runs/{run_id}/attempts/{attempt_number}",
+    //   {
+    //     owner,
+    //     repo,
+    //     run_id,
+    //     attempt_number,
+    //   }
+    // );
+    console.log("OWNER =>", owner);
+    console.log("REPO =>", repo);
 
-    const { context } = github;
+    // console.log(JSON.stringify(response.data, null, "\t"));
+
+    // const { context } = github;
 
     // core.setOutput("DATA", JSON.stringify(data));
     core.setOutput("repo", repo);
     core.setOutput("owner", owner);
     core.setOutput("run_id", run_id);
     core.setOutput("attempt_number", attempt_number);
-    core.setOutput("big_data", JSON.stringify(response.data));
+    // core.setOutput("big_data", JSON.stringify(response.data));
 
     core.startGroup("Logging github");
     console.log(JSON.stringify(github, null, "\t"));
     core.endGroup();
-
-    core.startGroup("Logging github context");
-    console.log(JSON.stringify(context, null, "\t"));
-    core.endGroup();
   } catch (error) {
     core.setFailed(error.message);
+    console.error(error);
   }
-})();
+}
+
+run();
 
 
 /***/ }),
--- .github/actions/custom/index.js
+++ .github/actions/custom/index.js
@@ -1,9 +1,10 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const { Octokit } = require("@octokit/core");
-(async () => {
+
+async function run() {
   try {
-    const token = core.getInput("GITHUB_TOKEN");
+    const token = core.getInput("token");
     const owner = core.getInput("owner");
     const repo = core.getInput("repo");
     const run_id = core.getInput("run_id");
@@ -16,34 +17,37 @@ const { Octokit } = require("@octokit/core");
     //   run_id,
     //   attempt_number,
     // });
-    const octokit = new Octokit({ auth: token });
-    const response = await octokit.request(
-      "GET /repos/{owner}/{repo}/actions/runs/{run_id}/attempts/{attempt_number}",
-      {
-        owner,
-        repo,
-        run_id,
-        attempt_number,
-      }
-    );
+    // const octokit = new Octokit({ auth: token });
+    // const response = await octokit.request(
+    //   "GET /repos/{owner}/{repo}/actions/runs/{run_id}/attempts/{attempt_number}",
+    //   {
+    //     owner,
+    //     repo,
+    //     run_id,
+    //     attempt_number,
+    //   }
+    // );
+    console.log("OWNER =>", owner);
+    console.log("REPO =>", repo);
 
-    const { context } = github;
+    // console.log(JSON.stringify(response.data, null, "\t"));
+
+    // const { context } = github;
 
     // core.setOutput("DATA", JSON.stringify(data));
     core.setOutput("repo", repo);
     core.setOutput("owner", owner);
     core.setOutput("run_id", run_id);
     core.setOutput("attempt_number", attempt_number);
-    core.setOutput("big_data", JSON.stringify(response.data));
+    // core.setOutput("big_data", JSON.stringify(response.data));
 
     core.startGroup("Logging github");
     console.log(JSON.stringify(github, null, "\t"));
     core.endGroup();
-
-    core.startGroup("Logging github context");
-    console.log(JSON.stringify(context, null, "\t"));
-    core.endGroup();
   } catch (error) {
     core.setFailed(error.message);
+    console.error(error);
   }
-})();
+}
+
+run();
