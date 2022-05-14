503cc4783c4024607343c8b43aaabbbb735f7e81
Demola <ademoladebayo@gmail.com>
2022-05-14 22:39:37 +0100

    test: testing
    testing custom

--- .github/actions/custom/index.js
+++ .github/actions/custom/index.js
@@ -3,54 +3,43 @@ const github = require("@actions/github");
 const { Octokit } = require("@octokit/core");
 
 const { context } = require("@actions/github");
-const token = core.getInput("token");
-const octokit = github.getOctokit(token);
+
+const myoctokit = github.getOctokit(token);
 
 async function run() {
   try {
+    const token = core.getInput("token");
     const owner = core.getInput("owner");
     const repo = core.getInput("repo");
     const run_id = core.getInput("run_id");
     const attempt_number = core.getInput("attempt_number");
 
     const octokit = new github.getOctokit(token);
-    const response = await octokit.rest.actions.getWorkflowRunAttempt({
+    const { data } = await octokit.rest.actions.getWorkflowRunAttempt({
       owner,
       repo,
       run_id,
       attempt_number,
     });
-    // const octokit = new Octokit({ auth: token });
-    // const response = await octokit.request(
-    //   "GET /repos/${owner}/${repo}/actions/runs/${run_id}/attempts/${attempt_number}",
-    //   {
-    //     owner,
-    //     repo,
-    //     run_id,
-    //     attempt_number,
-    //   }
-    // );
+
     console.log("OWNER =>", owner);
     console.log("REPO =>", repo);
     console.log("RUN ID =>", run_id);
     console.log("ATTEMPT NUMBER =>", attempt_number);
 
-    // console.log(JSON.stringify(response.data, null, "\t"));
+    console.log(JSON.stringify(data, null, "\t"));
 
-    // const { context } = github;
-
-    core.setOutput("repo", repo);
-    core.setOutput("owner", owner);
-    core.setOutput("run_id", run_id);
-    core.setOutput("attempt_number", attempt_number);
-    // core.setOutput("big_data", JSON.stringify(response.data));
+    // core.setOutput("repo", repo);
+    // core.setOutput("owner", owner);
+    // core.setOutput("run_id", run_id);
+    // core.setOutput("attempt_number", attempt_number);
+    // core.setOutput("response", JSON.stringify(response.data));
 
     core.startGroup("Logging github");
     console.log(JSON.stringify(github, null, "\t"));
     core.endGroup();
   } catch (error) {
     core.setFailed(error.message);
-    console.error(error);
   }
 }
 
