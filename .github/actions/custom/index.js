a484cad891d80c9172b0c8ff8f098fb4c4497ee9
Demola <ademoladebayo@gmail.com>
2022-05-15 14:08:20 +0100

    test: testing
    testing custom action

--- .github/actions/custom/index.js
+++ .github/actions/custom/index.js
@@ -6,19 +6,36 @@ const { context } = require("@actions/github");
 
 async function run() {
   try {
-    const token = core.getInput("token");
-    const owner = core.getInput("owner");
-    const repo = core.getInput("repo");
-    const run_id = core.getInput("run_id");
-    const attempt_number = core.getInput("attempt_number");
+    /**
+     * We need to fetch all the inputs that were provided to our action
+     * and store them in variables for us to use.
+     */
+    const owner = core.getInput("owner", { required: true });
+    const repo = core.getInput("repo", { required: true });
+    const run_id = core.getInput("run_id", { required: true });
+    const attempt_number = core.getInput("attempt_number", { required: true });
+    const token = core.getInput("token", { required: true });
 
+    /**
+     * Now we need to create an instance of Octokit which will use to call
+     * Github's REST API endpoints.
+     * We will pass the token as an argument to the constructor. This token
+     * will be used to authenticate our request.
+     */
     const octokit = new github.getOctokit(token);
-    // await octokit.rest.actions.getWorkflowRunAttempt({
+
+    // const response = await octokit.rest.actions.getWorkflowRunAttempt({
     //   owner,
     //   repo,
     //   run_id,
     //   attempt_number,
     // });
+
+    await octokit.rest.actions.getWorkflowRun({
+      owner,
+      repo,
+      run_id,
+    });
 
     console.log("OWNER =>", owner);
     console.log("REPO =>", repo);
@@ -27,10 +44,10 @@ async function run() {
 
     // console.log(JSON.stringify(data, null, "\t"));
 
-    // core.setOutput("repo", repo);
-    // core.setOutput("owner", owner);
-    // core.setOutput("run_id", run_id);
-    // core.setOutput("attempt_number", attempt_number);
+    core.setOutput("repo", repo);
+    core.setOutput("owner", owner);
+    core.setOutput("run_id", run_id);
+    core.setOutput("attempt_number", attempt_number);
     // core.setOutput("response", JSON.stringify(response.data));
 
     core.startGroup("Logging github");
